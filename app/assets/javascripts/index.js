a37af5214737bbfc8a4a863177b66466bd50e894
Cooper Dean <cooperdean30@gmail.com>
2015-03-03 18:28:07 -0800

    everything functional

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,17 +1,24 @@
 $(document).ready( function() {
 
 // TEXT DECORATION - NONE
-  $('a').css('text-decoration', 'none')
-  $('.ingli').val().toUpperCase();
+  $('a').css('text-decoration', 'none');
+ 
+  if($('.ingli').size() > 0) {
+    $('.ingli').val().toUpperCase();
+  }
 
 // SUBMIT
 
 $('#submit_box').on('click', function(){
   $('#shelf-container').slideUp('slow');
   $('#drink_container').slideDown('slow');
+});
 
+// EDIT
 
-
+$('#edit_box').on('click', function(){
+  $('#shelf-container').slideDown('slow');
+  $('#drink_container').slideUp('slow');
 });
 
 // INGREDIENT SEARCH
@@ -21,6 +28,15 @@ $('.ingli').each(function() {
     var txt = $(this).text();
     if (seen[txt])
         $(this).remove();
+    else
+        seen[txt] = true;
+});
+
+var seen = {};
+$('.liqtext').each(function() {
+    var txt = $(this).text();
+    if (seen[txt])
+        $(this).parent().remove();
     else
         seen[txt] = true;
 });
@@ -42,36 +58,92 @@ $('#ingredient_search').keyup(function(){
 // LIQBOX HOVER DELETE
   $('.liq-ing').hover( function () {
 
-    $(this).css('border-color', 'red')
+    $(this).css('border-color', 'red');
+    $(this).find('.remove_ingredient').fadeIn('fast');
 
   }, function () {
 
-    $(this).css('border-color', '#e8d136')
+    $(this).css('border-color', '#e8d136');
+    $(this).find('.remove_ingredient').fadeOut('fast');
 
   });
+
+// SUBMIT BOX VALUE
+var drinksCount = $('.liq-drink').size()
+
+
+if( drinksCount == 0 ) {
+$('#drink_counter').text("CLICK HERE WHEN YOU ARE FINISHED")
+}
+else {
+$('#drink_counter').text(drinksCount + " DRINKS TO VIEW")
+}
 
 // SUBMIT BOX HOVER
   $('#submit_box').hover( function () {
 
-    $(this).css('border-color', '#9AFE2E')
-    $(this).css('color', '#9AFE2E')
+    $(this).css('border-color', '#9AFE2E');
+    $(this).css('color', '#9AFE2E');
 
   }, function () {
 
-    $(this).css('border-color', '#e8d136')
-    $(this).css('color', 'white')
+    $(this).css('border-color', '#e8d136');
+    $(this).css('color', 'white');
+
+  });
+
+// EDIT BOX HOVER
+  $('#edit_box').hover( function () {
+
+    $(this).css('border-color', '#e8d136');
+    $(this).css('color', '#e8d136');
+
+  }, function () {
+
+    $(this).css('border-color', '#58ACFA');
+    $(this).css('color', 'white');
 
   });
 
 
-// NAVLINKS LINKS HOVER
-  $('a').hover( function () {
+// LINKS HOVER
+  $('.nav-header a').hover( function () {
 
-    $(this).css('color', '#e8d136')
+    $(this).css('color', '#e8d136');
 
   }, function () {
 
-    $(this).css('color', 'white')
+    $(this).css('color', 'white');
+
+  });
+
+  $('.shelf-ingredients a').hover( function () {
+
+    $(this).css('color', '#e8d136');
+
+  }, function () {
+
+    $(this).css('color', 'white');
+
+  });
+
+  $('.remove_ingredient a').hover( function () {
+
+    $(this).css('color', 'red');
+
+  }, function () {
+
+    $(this).css('color', 'white');
+
+  });
+
+  $('.drinktext a').hover( function () {
+
+    $(this).css('color', '#e8d136');
+
+  }, function () {
+
+    $(this).css('color', 'white');
 
   });
 
